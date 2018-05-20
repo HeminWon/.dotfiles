ff5ce0059fef337d87721eed8eec13f0453e3787
HeminWon <waiting4254@gmail.com>
2018-05-20 23:40:58 +0800

    add applist

--- scripts/backups.sh
+++ scripts/backups.sh
@@ -5,3 +5,18 @@ DOTFILES=$HOME/.dotfiles
 # backup Keyboard Maestro
 cp /$HOME/Library/Application\ Support/Keyboard\ Maestro/Keyboard\ Maestro\ Macro\ Stats.plist /$DOTFILES/Keyboard\ Maestro/Keyboard\ Maestro\ Macro\ Stats.plist
 cp /$HOME/Library/Application\ Support/Keyboard\ Maestro/Keyboard\ Maestro\ Macros.plist /$DOTFILES/Keyboard\ Maestro/Keyboard\ Maestro\ Macros.plist
+
+# backup app
+brew bundle dump --describe --force --file="/$DOTFILES/applist/Brewfile"
+
+# All Apps
+ls -lh /Applications > $DOTFILES/applist/All_AppList
+
+# MAS Apps
+/usr/local/bin/mas list > $DOTFILES/applist/MAS_AppList
+
+# brew Apps
+/usr/local/bin/brew list > $DOTFILES/applist/Brew_AppList
+
+# brew cask Apps
+/usr/local/bin/brew cask list > $DOTFILES/applist//BrewCask_AppList
