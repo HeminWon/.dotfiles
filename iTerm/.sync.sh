a0d5cdcd674a2857dfddf2d868d1fae19670e81b
HeminWon <waiting4254@gmail.com>
2018-06-03 22:16:52 +0800

    add scripts

---
+++ iTerm/.sync.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [[ ! -e /Applications/iTerm.app ]]; then
+    brew cask install iterm2
+else
+    echo "You have installed iTerm2"
+fi
+
+defaults delete com.googlecode.iterm2
+cp ~/dotfiles/iTerm/com.googlecode.iterm2.plist ~/Library/Preferences
