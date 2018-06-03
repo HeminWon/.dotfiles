2a3b86d5060b2671f42bb6feb00c977d19a3aeae
HeminWon <waiting4254@gmail.com>
2018-06-03 23:54:00 +0800

    update scripts

--- iTerm/.backup.sh
+++ iTerm/.backup.sh
@@ -8,9 +8,8 @@ backup_iTerm() {
 	NAME_ITERM=`ls ~/Library/Preferences | grep -E '^com.googlecode.iterm' | sort -V -r | head -n 1`
 	PATH_ITERM=~/Library/Preferences/$NAME_ITERM
 	if [[ -f "$PATH_ITERM" ]]; then
-		mkdir ~/dotfiles/iTerm && cp -R $PATH_ITERM ~/dotfiles/iTerm/$NAME_ITERM
+		cp -R $PATH_ITERM ~/dotfiles/iTerm/$NAME_ITERM
 	fi
-
 }
 
 backup_iTerm
