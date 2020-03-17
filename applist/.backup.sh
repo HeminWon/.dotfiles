abc9f23bd066e17f236625f10ceb19ba40b05962
Hemin Won <waiting4254@gmail.com>
2020-03-17 16:17:19 +0800

    backup

--- applist/.backup.sh
+++ applist/.backup.sh
@@ -7,16 +7,19 @@ backup_App() {
 	/usr/local/bin/brew bundle dump --describe --force --file="~/dotfiles/applist/Brewfile"
 
 	# All Apps
-	ls /Applications | sort -b > ~/dotfiles/applist/All_AppList
+	ls /Applications | sort -b > ~/dotfiles/applist/All_AppList.txt
 
 	# MAS Apps
-	/usr/local/bin/mas list | sort -b > ~/dotfiles/applist/MAS_AppList
+	/usr/local/bin/mas list | sort -b > ~/dotfiles/applist/MAS_AppList.txt
 
 	# brew Apps
-	/usr/local/bin/brew list | sort -b > ~/dotfiles/applist/Brew_AppList
+	/usr/local/bin/brew list | sort -b > ~/dotfiles/applist/Brew_AppList.txt
 
 	# brew cask Apps
-	/usr/local/bin/brew cask list | sort -b > ~/dotfiles/applist//BrewCask_AppList
+	/usr/local/bin/brew cask list | sort -b > ~/dotfiles/applist/BrewCask_AppList.txt
+
+	# screen saver
+	ls ~/Library/Screen\ Savers | sort -b > ~/dotfiles/applist/Savers_AppList.txt
 }
 
 backup_App
